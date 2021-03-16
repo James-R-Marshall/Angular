99cc4d4e38c5987727bd918312e30bed009c3bb5
james-R-marshall <slumburger15@gmail.com>
2021-03-16 00:46:23 -0700

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { QuoteComponent } from './quote/quote.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './login/login.component.js';
 import { ReccomendedComponent } from './reccomended/reccomended.component';
 import { FormsModule } from '@angular/forms';
 import { Quote } from '@angular/compiler';
